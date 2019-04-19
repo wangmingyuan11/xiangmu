af0ba4faa5654dd487bc28e1f4eb1fc9be4cae0d
Administrator <!5009267293@163.com>
2019-04-19 21:59:13 +0800

    1

---
+++ src/main/java/com/mmall/service/ICatgoryService.java
@@ -0,0 +1,13 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
+
+public interface ICatgoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+    ServerResponse updateCategoryName(Integer categoryId,String categoryName);
+    ServerResponse<List<Category>> getChildrenparallelCategory(Integer categoryId);
+    ServerResponse getCategoryAndDeepChildrenCategory(Integer categoryId);
+}
